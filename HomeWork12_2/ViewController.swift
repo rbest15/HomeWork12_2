9c67c7e97aac2c9a92b92d281643adf6dd1d31df
moremist <rbest15@rambler.ru>
2021-04-20 15:03:42 +0300

    Initial Commit

---
+++ HomeWork12_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork12_2
+//
+//  Created by Roman Kuzmich on 20.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
